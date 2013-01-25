92beebe8ab81d274ffd0bad3300bfc91be65efa0
Paul Redmond <paulrredmond@gmail.com>
2013-01-25 10:34:05 -0700

    Lib copyright info.

--- src/Purl/Autoloader.php
+++ src/Purl/Autoloader.php
@@ -1,9 +1,18 @@
 <?php
 
+/*
+ * This file is part of the Purl package, a project by Jonathan H. Wage.
+ *
+ * (c) 2013 Jonathan H. Wage
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Purl;
 
 /**
- * Purl class autoloader.
+ * Purl class autoloader
  */
 class Autoloader
 {
